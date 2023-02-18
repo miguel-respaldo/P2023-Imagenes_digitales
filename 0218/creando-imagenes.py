ab16a7023c99097c09df68afa01e850bad0eaddd
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2023-02-18 08:39:45 -0600

    otros rangos

--- 0218/creando-imagenes.py
+++ 0218/creando-imagenes.py
@@ -7,6 +7,14 @@ for filas in range(100):
     for columnas in range(100):
         img[filas][columnas] = 255
 
+#for filas in range(400, 500):
+#    for columnas in range(400, 500):
+#        img[filas][columnas] = 180
+
+for filas in range(100):
+    for columnas in range(100):
+        img[filas+400][columnas+400] = 180
+
 cv.imshow("Cuadro Negro", img)
 k = cv.waitKey(0)
 
