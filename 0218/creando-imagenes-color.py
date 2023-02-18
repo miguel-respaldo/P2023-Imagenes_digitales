23bfeac7ddc4a1afe97b3a317fb999a16bbb27d3
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2023-02-18 09:26:27 -0600

    colores cuadros

--- 0218/creando-imagenes-color.py
+++ 0218/creando-imagenes-color.py
@@ -5,9 +5,16 @@ img = np.zeros(shape=(500,500,3), dtype='uint8')
 
 for filas in range(100):
     for columnas in range(100):
-        img[filas][columnas][0] = 255
-        img[filas][columnas+100][1] = 255
-        img[filas][columnas+200][2] = 255
+        img[filas][columnas][0] = 255     # Blue
+        img[filas+400][columnas][1] = 255 # Green
+        img[filas][columnas+400][2] = 255 # Red
+        #Blanco
+        img[filas+400][columnas+400][0] = 255
+        img[filas+400][columnas+400][1] = 255
+        img[filas+400][columnas+400][2] = 255
 
 cv.imshow("Color", img)
 k = cv.waitKey(0)
+
+# Guardamos la imagen
+cv.imwrite("colores-cuadros.png",img)
