281b5b96474a64efc97d23de45001d53833700e4
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2023-02-18 08:23:27 -0600

    cuadrado blanco

--- 0218/creando-imagenes.py
+++ 0218/creando-imagenes.py
@@ -3,7 +3,10 @@ import numpy as np
 
 img = np.zeros(shape=(500,500), dtype='uint8')
 
-cv.imshow("Cuadro Negro", img)
+for filas in range(100):
+    for columnas in range(100):
+        img[filas][columnas] = 255
 
+cv.imshow("Cuadro Negro", img)
 k = cv.waitKey(0)
 
