968a5ad9cec4c62e859ce4f7ce991bd74e5f20c9
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2023-03-18 07:54:23 -0600

    comenzando

---
+++ 0318/ecualizacion_color.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Se lee la imagen
+img = cv.imread("../imgs/lenna.jpg")
+
+# Obtenemos su forma
+fil, col, ch = img.shape
+
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img_equa = cv.equalizeHist(img_gray)
+
+fig, ax = plt.subplots(2,2)
+ax[0][0].imshow(img_gray, cmap='gray', vmin=0, vmax=255)
+ax[0][1].hist(img_gray.ravel(),256,[0,256])
+ax[1][0].imshow(img_equa, cmap='gray', vmin=0, vmax=255)
+ax[1][1].hist(img_equa.ravel(),256,[0,256])
+plt.show()
